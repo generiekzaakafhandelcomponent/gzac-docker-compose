d9eb183cf621ca10a34d0a75c212765311bca0b1
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2025-08-28 14:57:40 +0200

    Fix open formulieren

--- imports/open-formulieren/database/fill-data-on-startup.sh
+++ imports/open-formulieren/database/fill-data-on-startup.sh
@@ -4,8 +4,8 @@ echo ">>>>  Waiting until Open Formulieren has initialized the database <<<<"
 useradd openforms
 while true
 do
-    verifier=$(psql -U openforms -d openforms -t -A -c "select count(jwt_valid_for) from zgw_consumers_service")
-    if [ "0" = $verifier ]
+    verifier=$(psql -U openforms -d openforms -t -A -c "select count(id) from auth_group")
+    if [ "0" != $verifier ]
         then
             echo "Running database setup scripts"
             for file in /docker-entrypoint-initdb.d/database/*.sql
